9611a81bdf1e896d5a223c9856d775e78192bb64
Sowmith Prajwal <sowmithprajwal@gmail.com>
2024-09-09 21:42:50 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+
+# Configuration for the database
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Define the Todo model
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+
+# Ensure the table is created
+with app.app_context():
+    db.create_all()
+
+@app.route('/', methods=['GET', 'POST'])
+def hello_world():
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo(title=title, desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+        return redirect('/')
+    
+    allToDo = Todo.query.all()
+    return render_template("index.html", allToDo=allToDo)
+
+@app.route('/show')
+def products():
+    allToDo = Todo.query.all()
+    print(allToDo)
+    return 'This is a products page!'
+
+@app.route('/update/<int:sno>', methods=['GET', 'POST'])
+def update(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo.title = title
+        todo.desc = desc
+        db.session.commit()
+        return redirect('/')
+    
+    return render_template("update.html", todo=todo)
+
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect('/')
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
